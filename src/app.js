c12322f7c46e9e68145af24ae764654f16b47329
cleyton.cabral <cleyton.cabral@ferreiracosta.com.br>
2024-01-20 22:07:22 -0300

    Rota de post e adição de leitura para json

--- src/app.js
+++ src/app.js
@@ -2,6 +2,9 @@ import express from "express";
 
 export const app = express();
 
+// expecificar para o exepress ler json
+app.use(express.json())
+
 // mock
 
 const selecoes = [
@@ -21,6 +24,15 @@ app.get("/selecoes", (req, res) => {
   try {
     res.status(200).send(selecoes);
   } catch (error) {
-    res.status(404).send("Oh, deu ruim!")
+    res.status(404).send("Oh, deu ruim!");
+  }
+});
+
+app.post("/selecoes", (req, res) => {
+  try {
+    selecoes.push(req.body);
+    res.status(201).send(req.body);
+  } catch (error) {
+    res.status(400).send({ message: error.message, data: "" });
   }
 });
