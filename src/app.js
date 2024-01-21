947f0b3369330a4dd3ec66860c464f93ad55ed74
cleyton.cabral <cleyton.cabral@ferreiracosta.com.br>
2024-01-21 14:01:03 -0300

    Criando rota de pegar por id e delete

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import express from "express";
 export const app = express();
 
 // expecificar para o exepress ler json
-app.use(express.json())
+app.use(express.json());
 
 // mock
 
@@ -13,6 +13,14 @@ const selecoes = [
   { id: 3, selecao: "Sérvia", grupo: "G" },
   { id: 4, selecao: "Camarões", grupo: "G" },
 ];
+
+const buscarSelecaoPorId = (id) => {
+  return selecoes.filter((selecao) => selecao.id == id);
+};
+
+const buscarIndexSelecao = (id) => {
+  return selecoes.findIndex((selecao) => selecao.id == id);
+};
 
 // criar rota padrão ou raiz
 
@@ -28,6 +36,20 @@ app.get("/selecoes", (req, res) => {
   }
 });
 
+// get por id
+app.get("/selecoes/:id", (req, res) => {
+  const index = req.params.id;
+
+  const verifyId = buscarSelecaoPorId(index);
+
+  try {
+    if (verifyId != null) res.json(verifyId);
+    return res.status(404).send({ message: error.message, data: "" });
+  } catch (error) {
+    res.status(400).send({ message: error.message, data: "" });
+  }
+});
+
 app.post("/selecoes", (req, res) => {
   try {
     selecoes.push(req.body);
@@ -35,4 +57,11 @@ app.post("/selecoes", (req, res) => {
   } catch (error) {
     res.status(400).send({ message: error.message, data: "" });
   }
+});
+
+app.delete("/selecoes/:id", (req, res) => {
+  const id = req.params.id;
+  const index = buscarIndexSelecao(id);
+  selecoes.splice(index, 1);
+  res.status(200).send(`Seleção com o id: ${id}, excluída com sucesso!`);
 });
