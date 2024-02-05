72a4476c7b441e811b8265ace69ffdef94fe62df
CleytonLu <cleytonlucasiurd@gmail.com>
2024-02-05 00:01:06 -0300

    Adição para lidar com exceções

--- src/server.js
+++ src/server.js
@@ -4,5 +4,9 @@ const port = process.env.PORT || 3000;
 
 // Escuta a execução da porta (isso inicia o back)
 app.listen(port, () => {
-  console.log(`Servidor rodando na porta: http://localhost:${port}`);
+  try {
+    console.log(`Servidor rodando na porta: http://localhost:${port}`);
+  } catch (error) {
+    console.log(`Deu erro na conexão do servidor, erro: ${error}`);
+  }
 });
